1f75f05843aa6fc2c123f45264dd493dd6acb5dc
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-11 15:29:41 -0800

    add Appointment model

---
+++ HairSalon/Models/Appointment.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HairSalon.Models
+{
+  public class Appointment
+  {
+    public int AppointmentId { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public virtual Client Client { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
